7b825678693931e375c60e77a13c1884050d1728
jihee Kim <jkim541@myseneca.ca>
2024-06-12 11:12:26 -0400

    Add profile dropdown menu

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
+import { Navbar, Nav, Dropdown } from 'react-bootstrap';
 import Image from 'next/image';
 import logo from '@/public/images/seneca-white.png';
 import profileDefault from '@/public/images/profile.png';
@@ -17,6 +17,16 @@ export default function MainNav() {
         <Nav className={styles.navLinks}>
           <Nav.Link href="/register" className={styles.navLink}>Register</Nav.Link>
           <Nav.Link href="/login" className={styles.navLink}>Login</Nav.Link>
+          <Dropdown align="end">
+            <Dropdown.Toggle variant="link" className={styles.navLink}>
+                <Image src={profileDefault} alt="User Profile" width={35} height={35} className={styles.profileImage} />
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item href="/profile">Your Profile</Dropdown.Item>
+              <Dropdown.Item href="/">Edit Profile</Dropdown.Item>
+              <Dropdown.Item href="/">Sign Out</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
