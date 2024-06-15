0314436251687bfe814d341071556302e559ddb4
Sunny Long <sunnylong44@gmail.com>
2024-06-15 16:40:39 -0400

    implement advisor page

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,29 +1,40 @@
-'use client';
-import { useState, useEffect } from 'react';
-import { Navbar, Nav, Dropdown } from 'react-bootstrap';
-import Image from 'next/image';
-import logo from '@/public/images/seneca-white.png';
-import profileDefault from '@/public/images/profile.png';
-import styles from '../styles/Home.module.css'; 
+"use client";
+import { useState, useEffect } from "react";
+import { Navbar, Nav, Dropdown } from "react-bootstrap";
+import Image from "next/image";
+import logo from "@/public/images/seneca-white.png";
+import profileDefault from "@/public/images/profile.png";
+import styles from "../styles/Home.module.css";
 
 export default function MainNav() {
   return (
     <Navbar className={styles.redNavbar} expand="lg">
       <Navbar.Brand href="/" className={styles.navbarBrand}>
-        <Image src={ logo } alt="Seneca Logo" width={140} height={35} />
+        <Image src={logo} alt="Seneca Logo" width={140} height={35} />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav" className={styles.navLinks}>
         <Nav className={styles.navLinks}>
-          <Nav.Link href="/register" className={styles.navLink}>Register</Nav.Link>
-          <Nav.Link href="/login" className={styles.navLink}>Login</Nav.Link>
+          <Nav.Link href="/register" className={styles.navLink}>
+            Register
+          </Nav.Link>
+          <Nav.Link href="/login" className={styles.navLink}>
+            Login
+          </Nav.Link>
           <Dropdown align="end">
             <Dropdown.Toggle variant="link" className={styles.navLink}>
-                <Image src={profileDefault} alt="User Profile" width={35} height={35} className={styles.profileImage} />
+              <Image
+                src={profileDefault}
+                alt="User Profile"
+                width={35}
+                height={35}
+                className={styles.profileImage}
+              />
             </Dropdown.Toggle>
             <Dropdown.Menu>
               <Dropdown.Item href="/profile">Your Profile</Dropdown.Item>
               <Dropdown.Item href="/editProfile">Edit Profile</Dropdown.Item>
+              <Dropdown.Item href="/advisor">Advisor</Dropdown.Item>
               <Dropdown.Item href="/">Sign Out</Dropdown.Item>
             </Dropdown.Menu>
           </Dropdown>
--- pages/advisor.js
+++ pages/advisor.js
@@ -1,45 +1,84 @@
 // pages/advisor.js
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
 
 export default function Advisor() {
   const router = useRouter();
   const [advisors, setAdvisors] = useState([
-    { id: 132125432, name: 'John Johnson', email: 'john.johnson@example.com', phone: '647-887-3654', sin: '123-456-789' },
-    { id: 221546454, name: 'James Smith', email: 'james.smith@example.com', phone: '416-553-9837', sin: '987-654-321' }
+    {
+      id: 132125432,
+      name: "John Johnson",
+      email: "john.johnson@example.com",
+      phone: "647-887-3654",
+      sin: "123-456-789",
+    },
+    {
+      id: 221546454,
+      name: "James Smith",
+      email: "james.smith@example.com",
+      phone: "416-553-9837",
+      sin: "987-654-321",
+    },
   ]);
 
   return (
-    <div>
-      <h1>Student Advisor</h1>
-      {advisors.map(advisor => (
-        <div key={advisor.id}>
-          <dl class="row">
-            <dt class="col-sm-2">ID:</dt>
-            <dd class="col-sm-10">{advisor.id}</dd>
-            <dt class="col-sm-2">Name:</dt>
-            <dd class="col-sm-10">{advisor.name}</dd>
-            <dt class="col-sm-2">Phone:</dt>
-            <dd class="col-sm-10">{advisor.phone}</dd>
-            <dt class="col-sm-2">Email:</dt>
-            <dd class="col-sm-10">{advisor.email}</dd>
-            <dt class="col-sm-2">SIN:</dt>
-            <dd class="col-sm-10">{advisor.sin}</dd>
-          </dl>
-        </div>
-      ))}
-
+    <div
+      className={styles.advisor}
+      style={{ display: "flex", justifyContent: "space-between" }}
+    >
       <div>
-            <dl class="row">
-                <dt class="col-sm-2">Student ID:</dt>
-                <dd class="col-sm-10">1329903213</dd>
+        <h1>Student Advisor</h1>
+        {advisors.map((advisor) => (
+          <div
+            className={styles.advisorInfo}
+            key={advisor.id}
+            style={{ display: "flex" }}
+          >
+            <Image
+              className={styles.advisorImage}
+              src="/images/profile.png"
+              alt="Advisor image"
+              width={180}
+              height={180}
+            />
+            <div>
+              <dl class="row">
+                <dt class="col-sm-2">ID:</dt>
+                <dd class="col-sm-10">{advisor.id}</dd>
+                <dt class="col-sm-2">Name:</dt>
+                <dd class="col-sm-10">{advisor.name}</dd>
+                <dt class="col-sm-2">Phone:</dt>
+                <dd class="col-sm-10">{advisor.phone}</dd>
                 <dt class="col-sm-2">Email:</dt>
-                <dd class="col-sm-10">sample@myseneca.ca</dd>
-                <dt class="col-sm-2">Address:</dt>
-                <dd class="col-sm-10">170 Finch Ave East</dd>
-                <dt class="col-sm-2">Phone Number:</dt>
-                <dd class="col-sm-10">(647)-892-9799</dd>
-            </dl>
+                <dd class="col-sm-10">{advisor.email}</dd>
+                <dt class="col-sm-2">SIN:</dt>
+                <dd class="col-sm-10">{advisor.sin}</dd>
+              </dl>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className={styles.studentInfo}>
+        <Image
+          className={styles.studentImage}
+          src="/images/profile.png"
+          alt="Advisor image"
+          width={180}
+          height={180}
+        />
+        <h4>Eric Yang</h4>
+        <dl class="row">
+          <dt class="col-sm-4">Student ID:</dt>
+          <dd class="col-sm-8">1329903213</dd>
+          <dt class="col-sm-4">Email:</dt>
+          <dd class="col-sm-8">sample@myseneca.ca</dd>
+          <dt class="col-sm-4">Address:</dt>
+          <dd class="col-sm-8">170 Finch Ave East</dd>
+          <dt class="col-sm-4">Phone Number:</dt>
+          <dd class="col-sm-8">(647)-892-9799</dd>
+        </dl>
       </div>
     </div>
   );
