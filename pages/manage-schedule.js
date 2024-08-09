a609556661106f8945ae2fbb3b93002a4dba163f
Sunny Long <yoursenecaid@myseneca.ca>
2024-08-09 16:33:31 -0400

    fix a small error

--- pages/manage-schedule.js
+++ pages/manage-schedule.js
@@ -11,9 +11,9 @@ export default function ManageSchedule() {
   const [selectedSection, setSelectedSection] = useState("");
   const [availability, setAvailability] = useState(null);
   const [eligibility, setEligibility] = useState(null);
-  const [sectionCode, setSectionCode] = useState('');
-  const [day, setDay] = useState('');
-  const [timeframe, setTimeframe] = useState('');
+  const [sections, setSections] = useState("");
+  const [day, setDay] = useState("");
+  const [timeframe, setTimeframe] = useState("");
 
   useEffect(() => {
     async function fetchData() {
@@ -121,7 +121,6 @@ export default function ManageSchedule() {
     }
   };
 
-
   return (
     <div className={styles.container}>
       <div className={styles.leftColumn}>
@@ -213,8 +212,14 @@ export default function ManageSchedule() {
             </ul>
           </div>
         )}
-        {availability === false && <p className={styles.error}>The selected course is not available.</p>}
-        {eligibility === false && <p className={styles.error}>The student is not eligible for the selected course.</p>}
+        {availability === false && (
+          <p className={styles.error}>The selected course is not available.</p>
+        )}
+        {eligibility === false && (
+          <p className={styles.error}>
+            The student is not eligible for the selected course.
+          </p>
+        )}
       </div>
     </div>
   );
